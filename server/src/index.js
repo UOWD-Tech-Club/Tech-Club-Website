110cc431ee6f97067ba2f01fd0030609812ce720
Hiba Anwar <84430693+Hynwar@users.noreply.github.com>
2024-09-21 22:17:28 +0400

    Initial commit

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+//set up env vars
+require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
