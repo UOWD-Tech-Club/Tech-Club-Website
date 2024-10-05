273325a10081827c7806b42115cf14728ed8de80
Mohammad Zain <mohammad.zain575@gmail.com>
2024-10-05 11:22:04 +0400

    feat(header): added a header file in the client side

---
+++ client/src/Header.jsx
@@ -0,0 +1,20 @@
+// A simple header component
+function Header() {
+  return (
+    <header>
+      <h1>Welcome to the Tech Club</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
