9a12c4d12c0c46e316f4794cbe65d071cf3af6f8
mariah <mariahkhalifa05@gmail.com>
2024-10-05 23:56:35 +0400

    feat(header): add a welcome header

---
+++ client/src/Header.jsx
@@ -0,0 +1,11 @@
+const header = () => {
+  return (
+    <header>
+      <h1>Welcome to the website</h1>
+      <img src="" alt="" />
+      <span>Last updated: 5/10/2024</span>
+    </header>
+  );
+};
+
+export default header;
