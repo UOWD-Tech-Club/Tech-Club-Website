dab59fedd550aa8419acfd7846e7375e83a3bf60
Rudraa <rudybbx3@gmail.com>
2024-10-13 17:16:48 +0400

    Server Config

---
+++ server/Models/event.js
@@ -0,0 +1,4 @@
+
+
+//File contains the code that is ran initially to create the Events table -- for reference only (Not in production)
+
---
+++ server/Models/users.js
@@ -0,0 +1,3 @@
+
+
+//File contains the code that is ran initially to create the Users table -- for reference (Not in production)
