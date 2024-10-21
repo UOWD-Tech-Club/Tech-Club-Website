e057bd6f1b1f5851af6c97efa825f3bffae31fc6
rudra-5 <rudralakhani2005@gmail.com>
2024-10-21 22:29:36 +0400

    Revert "added api route to fetch events"
    This reverts commit 1a1066d3d1ceb9412e2cb9633fa9122a95a75854.

--- server/Routes/eventsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getEvents } = require("../Controllers/Events");
-const router = express.Router();
-
-// Route to fetch events
-router.get("/events", getEvents);
-
-module.exports = router;
