4a6d3e9710a30db785a9339196887f60e8d43155
Hiba Anwar <hiba2anwar@gmail.com>
2025-02-16 18:11:41 +0400

    prep for production

---
+++ client/src/pages/AboutPage.jsx
@@ -0,0 +1,11 @@
+import PageLayout from '../layout/PageLayout';
+
+function AboutPage() {
+  return (
+    <PageLayout>
+      <h1 style={{ height: '100vh' }}>About Us Page</h1>
+    </PageLayout>
+  );
+}
+
+export default AboutPage;
