e4a3951c8522a8a96f76fa5f4b18b85302b033c5
Mariah0_0 <mariahkhalifa05@gmail.com>
2025-02-21 23:09:48 +0400

    fix(NewsPage): improve ui

--- client/src/pages/NewsPage.jsx
+++ client/src/pages/NewsPage.jsx
@@ -29,15 +29,15 @@ function NewsPage() {
       const allNews = data.news;
 
       if (allNews && allNews.length > 0) {
-        setLatestNews(allNews.slice(0, 1));
+        setLatestNews(allNews[0]);
         setNewsItems(allNews.slice(1));
       } else {
-        setLatestNews([]);
+        setLatestNews();
         setNewsItems([]);
       }
     } catch (error) {
       console.error('Error fetching news:', error);
-      setLatestNews([]);
+      setLatestNews();
       setNewsItems([]);
     } finally {
       setIsLoading(false);
@@ -48,16 +48,6 @@ function NewsPage() {
     fetchNews();
   }, [filter]); // Add filter as dependency
 
-  if (isLoading) {
-    return (
-      <PageLayout>
-        <div className={styles.newsContainer}>
-          <div>Loading...</div>
-        </div>
-      </PageLayout>
-    );
-  }
-
   return (
     <PageLayout>
       <div className={styles.newsContainer}>
@@ -65,81 +55,103 @@ function NewsPage() {
           <h3>Filter : </h3>
           <div className={styles.filterOptions}>
             <button
-              className={styles.filterButton}
+              className={`${styles.filterButton} ${filter === 'techClubNews' && styles.active} `}
               onClick={() => setFilter('techClubNews')}
             >
               Tech Club
             </button>
             <button
-              className={styles.filterButton}
+              className={`${styles.filterButton} ${filter === 'dailyNews' && styles.active}`}
               onClick={() => setFilter('dailyNews')}
             >
               Daily News
             </button>
           </div>
         </div>
-
-        <div className={styles.featuredNews}>
-          {latestNews.length > 0 &&
-            latestNews.map((news) => (
-              <div key={news.news_id} className={styles.newsItem}>
-                <div className={styles.imageWrapper}>
-                  <img
-                    src={news.news_img}
-                    alt={news.news_title}
-                    className={styles.featuredImage}
-                  />
-                </div>
-                <div className={styles.featuredContent}>
-                  <span className={styles.tag}>
-                    {news.news_category || ' '}
-                  </span>
-                  <h2 className={styles.featuredTitle}>{news.news_title}</h2>
-                  <p className={styles.featuredExcerpt}>
-                    {news.news_description}
-                  </p>
-                  <div className={styles.meta}>
-                    <span className={styles.date}>
-                      {new Date(news.news_pubdate).toLocaleDateString()}
-                    </span>
-                    <span className={styles.author}>
-                      • by {news.news_source}
-                    </span>
-                  </div>
-                </div>
-              </div>
-            ))}
-        </div>
-
-        <h1 className={styles.sectionTitle}>Latest News</h1>
-
-        <div className={styles.newsGrid}>
-          {newsItems.map((news) => (
-            <article key={news.news_id} className={styles.newsCard}>
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <>
+            {/* Featured News Section */}
+            <a
+              key={latestNews.news_id}
+              href={latestNews.news_url}
+              className={styles.featuredNews}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <div className={styles.imageWrapper}>
                 <img
-                  src={news.news_img}
-                  alt={news.news_title}
-                  className={styles.newsImage}
+                  src={latestNews.news_img}
+                  alt={latestNews.news_title}
+                  className={styles.featuredImage}
                 />
-                <a href={news.news_url} className={styles.readMore}>
-                  Read More
-                </a>
+                <div className={styles.readMore}>Read More</div>
               </div>
-              <div className={styles.newsContent}>
-                <span className={styles.tag}>{news.news_category || ' '}</span>
-                <h3 className={styles.newsTitle}>{news.news_title}</h3>
-                <p className={styles.newsExcerpt}>{news.news_description}</p>
+              <div className={styles.featuredContent}>
+                <span className={styles.tag}>
+                  {latestNews.news_category ||
+                    (filter === 'dailyNews' && 'Daily News')}
+                </span>
+                <h2 className={styles.featuredTitle}>
+                  {latestNews.news_title}
+                </h2>
+                <p className={styles.featuredExcerpt}>
+                  {latestNews.news_description}
+                </p>
                 <div className={styles.meta}>
                   <span className={styles.date}>
-                    {format(new Date(news.news_pubdate), 'd MMMM, yyyy')}
+                    {new Date(latestNews.news_pubdate).toLocaleDateString()}
                   </span>
-                  <span className={styles.author}>• by {news.news_source}</span>
+                  <span className={styles.author}>
+                    • by {latestNews.news_source}
+                  </span>
                 </div>
               </div>
-            </article>
-          ))}
-        </div>
+            </a>
+
+            {/* Latest News Section */}
+            <h1 className={styles.sectionTitle}>Latest News</h1>
+            <div className={styles.newsGrid}>
+              {newsItems.map((news) => (
+                <a
+                  key={news.news_id}
+                  href={news.news_url}
+                  className={styles.newsCard}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className={styles.imageWrapper}>
+                    <img
+                      src={news.news_img}
+                      alt={news.news_title}
+                      className={styles.newsImage}
+                    />
+                    <div className={styles.readMore}>Read More</div>
+                  </div>
+                  <div className={styles.newsContent}>
+                    <span className={styles.tag}>
+                      {news.news_category ||
+                        (filter === 'dailyNews' && 'Daily News')}
+                    </span>
+                    <h3 className={styles.newsTitle}>{news.news_title}</h3>
+                    <p className={styles.newsExcerpt}>
+                      {news.news_description}
+                    </p>
+                    <div className={styles.meta}>
+                      <span className={styles.date}>
+                        {format(new Date(news.news_pubdate), 'd MMMM, yyyy')}
+                      </span>
+                      <span className={styles.author}>
+                        • by {news.news_source}
+                      </span>
+                    </div>
+                  </div>
+                </a>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </PageLayout>
   );
