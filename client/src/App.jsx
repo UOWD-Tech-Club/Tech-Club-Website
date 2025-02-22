80d2c5c9d37ad514c374e0bf6a4170a1323fe2ac
Maw1z <mohdmawiz11@gmail.com>
2025-02-22 14:06:59 +0400

    Add scroll to top feature upon navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import ProjectsPage from './pages/ProjectsPage';
 import NewsPage from './pages/NewsPage';
 import NewsArticle from './pages/NewsArticle';
 import { Routes, Route } from 'react-router-dom';
+import ScrollToTop from './utils/ScrollToTop';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <span className="banner">
         This site is in beta—expect improvements and occasional bugs
       </span>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/events/:eventId" element={<EventsPage />} />
