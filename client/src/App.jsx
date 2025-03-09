f5effd072e8bdf0f78a02b088fed0fdcb14a028c
Bilal <bilalsxj@gmail.com>
2025-03-09 13:41:24 +0400

    Added About Us page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import Homepage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage';
 import NewsPage from './pages/NewsPage';
 import NewsArticle from './pages/NewsArticle';
+import AboutUsPage from './pages/AboutUsPage';
+import AllExecutives from './pages/AllExecutives';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -18,6 +20,8 @@ function App() {
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/newsletter" element={<NewsPage />} />
         <Route path="/newsarticle" element={<NewsArticle />} />
+        <Route path="/aboutus" element={<AboutUsPage />} />
+        <Route path="/all-executives" element={<AllExecutives />} />
       </Routes>
     </>
   );
