81800dfd18713a94053fa658b00b4252d4b3a569
MegaliOrexi <mohammad.zain575@gmail.com>
2025-04-26 02:50:27 +0400

    Sidebar error renamed

--- client/src/layout/SideBar.jsx
+++ client/src/layout/SideBar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import styles from './Sidebar.module.css';
+import styles from './SideBar.module.css';
 
 function Sidebar() {
   const location = useLocation();
