302113f2f4241c71ee8aeb0ddb7a13c6d2545ae7
Maw1z <mohdmawiz11@gmail.com>
2025-05-17 20:06:23 +0400

    Add backend for executives page

---
+++ server/Routes/executivesManagementRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  inviteExecutive,
+  getAllExecutives,
+  updateExecutiveRole,
+  getExecutive,
+  deleteExecutive,
+} from "../Controllers/executivesManagement.js";
+
+const router = express.Router();
+
+router.post("/executives/invite", inviteExecutive);
+router.get("/executives", getAllExecutives);
+router.get("/executives/:executiveID", getExecutive);
+router.put("/executives/:executiveID", updateExecutiveRole);
+router.delete("/executives/:executiveID", deleteExecutive);
+
+export default router;
--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import eventsRoutes from "./Routes/eventRoutes.js";
 import newsRoutes from "./Routes/newsRoutes.js";
 import "./Tasks/scheduled.js";
 import newsManagementRoutes from "./Routes/newsManagementRoutes.js";
 import authRoutes from "./Routes/authRoutes.js";
 import eventManagementRoutes from "./Routes/eventManagementRoutes.js";
+import executiveManagementRoutes from "./Routes/executivesManagementRoutes.js";
 //ES6 equivalent to __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -26,12 +27,14 @@ import "./Tasks/scheduled.js";
 const app = express();
 
 // Configure CORS
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(cookieParser());
 
@@ -41,6 +44,7 @@ app.use("/events", eventsRoutes);
 app.use("/news", newsRoutes);
 app.use("/newsManagement", newsManagementRoutes);
 app.use("/eventManagement", eventManagementRoutes);
+app.use("/executivesManagement", executiveManagementRoutes);
 app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
