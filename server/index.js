46e525d39152c4e8e9d644bbf8403ae5aea9366d
MegaliOrexi <mohammad.zain575@gmail.com>
2025-05-20 18:55:09 +0400

    Fix(index.js): Allow both localhost and uowdtechclub to acess routes

--- server/index.js
+++ server/index.js
@@ -26,10 +26,22 @@ import "./Tasks/scheduled.js";
 
 const app = express();
 
-// Configure CORS
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://www.uowdtechclub.com",
+];
+
 app.use(
   cors({
-    origin: "https://www.uowdtechclub.com",
+    origin: (origin, callback) => {
+      // Allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
